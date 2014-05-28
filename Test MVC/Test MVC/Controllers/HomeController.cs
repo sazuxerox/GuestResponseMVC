9b1275d1b5f26f84f1683646194b08b94c7b0a83
Sazzad Hossain <sazzadhossain@live.com>
2014-05-28 15:31:13 +0600

    All my files

---
+++ Test MVC/Test MVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_MVC.Models;
+
+namespace Test_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour < 12)
+                @ViewBag.Greeting = "Good Morning, Have a nice day";
+            else
+                @ViewBag.Greeting = "Good Afternoon, Have a good time";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //ToDo: Email Response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //There is a validation error
+                return View();
+            }
+        }
+    }
+}
---
+++ Test MVC/Test MVC/Models/GuestResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test_MVC.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage="Please enter your email address")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage="Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Please enter your phone number")]
+        [RegularExpression(@"^([+0][8][8])?[0][1][15-9]\d{8}$", ErrorMessage="Please enter a valid mobile number like +8801XXXXXXXX or 08801XXXXXXXXX or 01XXXXXXXXX")]
+        public string Phone { get; set; }
+        
+        [Required(ErrorMessage="Please specify if you will come or not!")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
